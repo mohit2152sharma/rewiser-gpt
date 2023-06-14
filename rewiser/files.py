ea621c5c532c81a8ba124b83505f9a84f89a66cd
Mohit Sharma <mohitsharma@Mohits-MacBook-Pro.local>
2023-06-14 20:37:54 +0530

    fixing filepath

--- rewiser/files.py
+++ rewiser/files.py
@@ -34,7 +34,7 @@ def list_files(
 
 def get_commit_date(filepath: str) -> str:
     # cmnd = f'git --no-pager log -1 --format=%cd "{filepath}"'
-    cmnd = ["git", "--no-pager", "log", "-1", "--format=%cd", f'"{filepath}"']
+    cmnd = ["git", "--no-pager", "log", "-1", "--format=%cd", "--", f'"{filepath}"']
     date_str = subprocess.check_output(cmnd)
     date_str = date_str.decode("utf-8").strip()
     # cmnd_output = subprocess.run(
