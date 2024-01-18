f05601c0264809517adb748ea5698a632b990d2b
mohit sharma <mohitlakshya@gmail.com>
2024-01-18 16:10:12 +0530

    trying different algo

--- rewiser/anki.py
+++ rewiser/anki.py
@@ -4,6 +4,8 @@ from typing import List
 
 from rewiser.utils import file_commit_date
 
+EF = 2.5
+
 
 def pseudo_anki(filenames: List[str]) -> List[str]:
     """Given a the sorted list of files, decides which one to include to send.
@@ -22,18 +24,35 @@ def pseudo_anki(filenames: List[str]) -> List[str]:
     # select files matching the date s
 
     current_date = datetime.utcnow().date()
-    first_file = filenames[-1]
+    # first_file = filenames[-1]
+    #
+    # min_date = file_commit_date(first_file)
+    # min_date = datetime.strptime(min_date, "%Y-%m-%d").date()
+    #
+    # dates_to_send = []
+    # date_variable = current_date
+    # i = 0
+    # while current_date - timedelta(2 * i) > min_date:
+    #     date_variable = current_date - timedelta(2 * i)
+    #     dates_to_send.append(date_variable.strftime("%Y-%m-%d"))
+    #     i += 1
+    # result = [f for f in filenames if file_commit_date(f) in dates_to_send]
+    # logging.info(f"files selected: {result}")
+    # return result
 
-    min_date = file_commit_date(first_file)
-    min_date = datetime.strptime(min_date, "%Y-%m-%d").date()
+    result = []
+    for file in filenames:
+        d = datetime.strptime(file_commit_date(file), "%Y-%m-%d").date()
 
-    dates_to_send = []
-    date_variable = current_date
-    i = 0
-    while current_date - timedelta(2 * i) > min_date:
-        date_variable = current_date - timedelta(2 * i)
-        dates_to_send.append(date_variable.strftime("%Y-%m-%d"))
-        i += 1
-    result = [f for f in filenames if file_commit_date(f) in dates_to_send]
+        revision_date = d + timedelta(days=1)
+        delta = 1
+        revision_dates = [revision_date.strftime("%Y-%m-%d")]
+        while revision_date < current_date:
+            new_date = revision_date + timedelta(days=delta * EF)
+            revision_dates.append(new_date.strftime("%Y-%m-%d"))
+            delta = new_date - revision_date
+            revision_date = new_date
+        if current_date in revision_dates:
+            result.append(file)
     logging.info(f"files selected: {result}")
     return result
