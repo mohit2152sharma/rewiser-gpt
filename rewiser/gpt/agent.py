b7539221e555d156b116cea6ad5d729c3e2a1410
mohit sharma <mohitlakshya@gmail.com>
2024-08-25 15:58:13 +0530

    adding gp4-o model

--- rewiser/gpt/agent.py
+++ rewiser/gpt/agent.py
@@ -1,15 +1,17 @@
-import traceback
 import logging
+import traceback
+
+from langchain import LLMChain, PromptTemplate
+from langchain.llms import OpenAI
+
 from rewiser.gpt.utils import create_prompt_template
 from rewiser.utils import read_env_var
-from langchain.llms import OpenAI
-from langchain import LLMChain, PromptTemplate
 
 
 class OpenAIAgent:
     def __init__(self, template_name: str, openai_key: str | None = None) -> None:
         self.openai_key = openai_key or read_env_var("OPENAI_API_KEY", raise_error=True)
-        self.llm = OpenAI(openai_api_key=self.openai_key)  # type: ignore
+        self.llm = OpenAI(openai_api_key=self.openai_key, model="gpt-4o")  # type: ignore
         self.template_name = template_name
 
     def create_template(self) -> PromptTemplate:
